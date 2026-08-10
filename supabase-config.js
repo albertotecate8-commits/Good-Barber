bd23a33ad703fab7b4a04ce092e1caec92069f69
albertotecate8-commits <albertotecate8@gmail.com>
2026-08-09 20:45:36 -0600

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://smswcsbxefeeugabqfb.supabase.co";
-const SUPABASE_KEY = "TU_CLAVE_PUBLISHABLE";
+const SUPABASE_KEY = "sb_publishable_IyoPemj_PIq71ykyF5LHoA_RJEvFKTb";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
